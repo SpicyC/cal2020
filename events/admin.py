15fad956227a8181d275efd07c7c83ae8fbf2e7d
SpicyC <whitecinamon@gmail.com>
2020-08-09 23:30:22 +0000

    Initial commit

---
+++ events/admin.py
@@ -0,0 +1,17 @@
+# from django.contrib import admin
+
+# Register your models here.
+
+
+
+
+from __future__ import unicode_literals
+ 
+from django.contrib import admin
+from .models import Event
+ 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['day', 'start_time', 'end_time', 'notes']
+
+
+admin.site.register(Event)
---
+++ events/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+
+from .models import Event
+
+# Create your views here.
+
